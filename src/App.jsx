f9570d2d42593be45166527645b40c7283961340
TinWinHlaing-777 <tinwinhlaingmm777@gmail.com>
2023-08-03 20:08:30 +0630

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { RouterProvider } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider/>
+    </>
+  )
+}
+
+export default App
